97c41043b9113b55256043d78d5048f6718a27c1
ioetbc <ioetbc@gmail.com>
2022-03-17 11:19:40 +0000

    get html showing and console.log

---
+++ README.md
@@ -0,0 +1,13 @@
+# rubber-ducker-2 README
+
+## Run the extension locally
+
+### What files do
+
+## Run the API locally
+
+`RubberDuckerPanel` a file for...
+
+## Tips
+
+open developer tools in the VSCODE window to debug UI `cmd shift p` `Open Webview Developer Tolls`
---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { RubberDuckerPanel } from "./RubberDuckerPanel";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rubber-ducker-2.helloWorld", () => {
+      RubberDuckerPanel.createOrShow(context.extensionUri);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rubber-ducker-2.doSomething", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "this is somehting coool",
+        "good",
+        "bad"
+      );
+
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("soz");
+      }
+
+      console.log(answer);
+    })
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
